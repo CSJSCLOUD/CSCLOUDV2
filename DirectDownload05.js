3063e6ae19ed8131624f523c6cc68680a0b7a788
cyberdevlk <138901572+cyberdevlk@users.noreply.github.com>
2024-11-30 19:59:26 +0530

    Update DirectDownload05.js

--- DirectDownload05.js
+++ DirectDownload05.js
@@ -1,10 +1,11 @@
 [
 {
 "from" : "ima04.cskinglk.xyz/server4/new/",
-"to" : "cdn41.janathaholdings.xyz/cloud04drive01/NEW/"
+"to" : "new.cscloud12.online/cloud04drive01/NEW/"
 },
 {
 "from" : "ima04.cskinglk.xyz/server4/new2/",
-"to" : "cdn42.janathaholdings.xyz/cloud04drive01/NEW/"
+"to" : "new2.cscloud12.online/cloud04drive01/NEW/"
 }
 ]
+
